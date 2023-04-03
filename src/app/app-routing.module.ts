9654e39cdab38e0f9d0ea0c2fbe144d669b04ffd
Miguel Monteiro <miguelmonteiroclaveri@gmail.com>
2023-04-03 20:04:11 +0200

    Made connection between both mfes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'simple-mfe',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'simple-mfe',
+        exposedModule: './SimpleModuleModule',
+      }).then((m) => m.SimpleModuleModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
